e327c4c7d9d96a95cdf3398b64f4241d6b762e7c
RZ2017 <abolfazl.razi@nau.edu>
2017-06-06 14:13:51 -0700

    Add files via upload

--- NFutil/setting.hh
+++ NFutil/setting.hh
@@ -47,15 +47,19 @@
 #define RAZI_DEBUG 			(SHOW_FIRE | SHOW_RHS | SHOW_SIM) //| CREATE_REACTION| CREATE_FUNC
 
 
-#define TEST_REAC   "XWBindCond1"//"XYBind"//"Bug1"//"XaYbBind" //Razi:Show more messages for reactions: Use "ALL" to show all reactions
-#define RAZI_VER   1 //Razi: activates some debugging messages
-#define FIX_BUG1 1   //Razi: this applies some changes to fix the local function bug
+#define TEST_REAC   	"XWBindCond1"//"XYBind"//"Bug1"//"XaYbBind" //Razi:Show more messages for reactions: Use "ALL" to show all reactions
+#define RAZI_VER   1     //Razi: activates some debugging messages
+#define FIX_BUG1 1      //Razi: this applies some changes to fix the local function bug
+
+
+#define RHS_FUNC   	//Razi: Activates function evaluation in the output
+   	   	   	   	    //Razi: Also check the products and avoid firing a reaction whose products are connected
+						// The products should be disjoint complexes
+
+
+
+
 //#define FIX_OBS_COUNT //Razi: Activate later after checking the results. This macro corrects the problem of counting a molecule twice against an observable: EX: X(P~Phos) for OBS: X(p~Phos,w!1).W(a!1,b!2).X(p~Phos,w!2)
-
-#define RHS_FUNC 1  //Razi: Activates function evaluation in the output
-#define RHS_maxRETRY  1  //define number of retries for reactions with RHS functions
-
-
 //#define FIX_A     1 //Razi: Change propensity update for reactions, and calculating reaction fire time [dt]
 /* Razi: Later check, but it seems that there is a problem in calculating propensities. In the original version the propensity of a reaction is calculated
  * as base rate * n1*n2*..nk, where ni is the number of  the ith  reactant
